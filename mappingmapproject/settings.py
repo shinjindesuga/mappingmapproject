3266bf7528bc7bb7ef8b71945bfc6bee1bc7a5c4
shinjindesuga <59989513+shinjindesuga@users.noreply.github.com>
2020-06-06 18:11:37 +0900

    細部を修正しました

--- mappingmapproject/settings.py
+++ mappingmapproject/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'mappingmapproject.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
+    #    'ENGINE': 'django.db.backends.sqlite3',
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
           'NAME': 'mappingmap',
           'USER': 'koki',
           'PASSWORD': 'Geco5803',
