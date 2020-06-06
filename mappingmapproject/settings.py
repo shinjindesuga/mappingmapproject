3a2cdfee2738b0c961f4c499edb3cfbcef000ddf
Ubuntu <ubuntu@ip-172-31-11-3.ap-northeast-1.compute.internal>
2020-06-06 08:51:11 +0000

    データベースの変更を行いました

--- mappingmapproject/settings.py
+++ mappingmapproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's1e1!(p(7s!uv+yzl%eww36*mbt!(g706@i6@+9-x@3=!7&h86'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.180.173.39']
 
 
 # Application definition
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'mappingmapproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+          'NAME': 'mappingmap',
+          'USER': 'koki',
+          'PASSWORD': 'Geco5803',
+          'HOST': 'localhost',
+          'PORT': '',
     }
 }
 
